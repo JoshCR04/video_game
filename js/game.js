4eb0590b9659f1082fd2869dab2d85c324edec14
JoshCR04 <joshua.cerdas@ucr.ac.cr>
2024-10-22 05:00:28 -0600

    otra prueba

--- js/game.js
+++ js/game.js
@@ -11,37 +11,55 @@ gameScene.init = function () {
 
     // Datos del suelo
     this.gameObjects = [
-        { type: 'ground_1', x: 0.5, y: -65 },
-        { type: 'ground_2', x: 0.5, y: -65 },
-        { type: 'ground_3', x: 0.5, y: -65 },
-        { type: 'ground_4', x: 0.5, y: -72 },
-        { type: 'ground_4', x: 0.5, y: -72 }
+        { type: 'ground_1', x: 390, y: -65 },
+        { type: 'ground_2', x: 1485, y: -65 },
+        { type: 'ground_3', x: 2648, y: -65 },
+        { type: 'ground_4', x: 3937, y: -72 },
+        { type: 'ground_4', x: 5408, y: -72 }
     ];
 
     // Datos de enemigos
     this.enemiesObjects = [
-        { type: 'flying_bug', x: 0.5, y: -600 },
-        { type: 'flying_bug', x: 0.5, y: -300 },
-        { type: 'witch', x: 0.5, y: -350 },
-        { type: 'monster', x: 0.5, y: -300 }
+        { type: 'flying_bug', x: 920, y: -600 },
+        { type: 'flying_bug', x: 790, y: -300 },
+        { type: 'witch', x: 2420, y: -350 },
+        { type: 'monster', x: 4120, y: -300 }
     ];
 
-    // Assets
+    //assets
     this.gameItems = [
-        { type: 'mushroom', x: 0.5, y: -190 },
-        { type: 'key', x: 0.5, y: -180 },
-        { type: 'crow', x: 0.5, y: -510 },
-        { type: 'goblin', x: 0.5, y: -331 },
-        { type: 'sword', x: 0.5, y: -500 },
-        { type: 'bread', x: 0.5, y: -175 }
+        { type: 'mushroom', x: 4000, y: -190 },
+        { type: 'key', x: 5400, y: -180 },
+        { type: 'crow', x: 2960, y: -510 },
+        { type: 'goblin', x: 1249, y: -331 },
+        { type: 'sword', x: 40, y: -500 },
+        { type: 'bread', x: 1105, y: -175 }
     ];
 
     // Datos de plataformas
     this.platforms = [
-        { type: 'platform_1', x: 0.5, y: -310 },
-        { type: 'platform_1', x: 0.5, y: -310 },
-        { type: 'platform_1', x: 0.5, y: -310 },
-        // Añadir más plataformas según sea necesario
+        { type: 'platform_1', x: 47, y: -310 },
+        { type: 'platform_1', x: 147, y: -310 },
+        { type: 'platform_1', x: 247, y: -310 },
+        { type: 'platform_1', x: 347, y: -310 },
+        { type: 'platform_1', x: 447, y: -310 },
+        { type: 'platform_1', x: 647, y: -215 },
+        { type: 'platform_1', x: 247, y: -510 },
+        { type: 'platform_1', x: 147, y: -380 },
+        { type: 'platform_1', x: 47, y: -450 },
+        { type: 'platform_1', x: 547, y: -510 },
+        { type: 'platform_1', x: 920, y: -330 },
+        { type: 'platform_1', x: 1020, y: -360 },
+        { type: 'platform_1', x: 1105, y: -430 },
+        { type: 'platform_1', x: 1290, y: -530 },
+        { type: 'platform_1', x: 1105, y: -130 },
+        { type: 'platform_1', x: 1290, y: -310 },
+        { type: 'platform_1', x: 1890, y: -100 },
+        { type: 'platform_1', x: 2110, y: -190 },
+        { type: 'platform_1', x: 3200, y: -200 },
+        { type: 'platform_2', x: 3410, y: -100 },
+        { type: 'platform_2', x: 4510, y: -140 },
+        { type: 'platform_2', x: 4810, y: -140 }
     ];
 };
 
@@ -80,28 +98,29 @@ gameScene.initGameObjects = function (fondo) {
     // Se crean los suelos
     this.grounds = this.physics.add.staticGroup();
     this.gameObjects.forEach(obj => {
-        this.grounds.create(this.scale.width * obj.x, fondo.height + obj.y, obj.type).setScale(1).refreshBody();
+        this.grounds.create(obj.x, fondo.height + obj.y, obj.type).setScale(1).refreshBody();
     });
 
     // Crea enemigos
     this.enemiesGroup = this.physics.add.group();
     this.enemiesObjects.forEach(enemy => {
-        let newEnemy = this.enemiesGroup.create(this.scale.width * enemy.x, fondo.height + enemy.y, enemy.type);
+        let newEnemy = this.enemiesGroup.create(enemy.x, fondo.height + enemy.y, enemy.type);
         newEnemy.body.setAllowGravity(enemy.type !== 'flying_bug');
         newEnemy.setCollideWorldBounds(true);
         newEnemy.body.setSize(newEnemy.width, newEnemy.height - 13, true);
     });
 
+
     // Crear las plataformas
     this.platformGroup = this.physics.add.staticGroup();
     this.platforms.forEach(plat => {
-        this.platformGroup.create(this.scale.width * plat.x, fondo.height + plat.y, plat.type).setScale(1).refreshBody();
+        this.platformGroup.create(plat.x, fondo.height + plat.y, plat.type).setScale(1).refreshBody();
     });
 
     // Objetos del juego (llave, espada, pan, hongo, duende, cuervo)
     this.gameItemsGroup = this.physics.add.staticGroup();
     this.gameItems.forEach(item => {
-        this.gameItemsGroup.create(this.scale.width * item.x, fondo.height + item.y, item.type).setScale(1).refreshBody();
+        this.gameItemsGroup.create(item.x, fondo.height + item.y, item.type).setScale(1).refreshBody();
     });
 };
 
@@ -135,7 +154,6 @@ gameScene.create = function () {
     // Control de teclas
     this.cursors = this.input.keyboard.createCursorKeys();
 };
-
 // Función para actualizar, realiza movimientos del personaje
 gameScene.update = function () {
     if (!this.cursors) return;
@@ -163,8 +181,8 @@ gameScene.update = function () {
 // Configuración del juego
 let config = {
     type: Phaser.AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: window.innerWidth >= 768 ? 5760 : window.innerWidth, // Ancho total si es pantalla grande
+    height: window.innerWidth >= 768 ? window.innerHeight : window.innerHeight * (window.innerWidth / 5760), // Ajuste de altura para pantallas pequeñas
     scene: gameScene,
     title: 'Sword Of Destiny - video_game',
     physics: {
@@ -173,20 +191,17 @@ let config = {
             gravity: { y: 900 },
             debug: false
         }
-    },
+    }
 };
 
-// Inicia el juego
+// Creación del juego
 let game = new Phaser.Game(config);
 
-// Manejo del evento de redimensionamiento de la ventana
+// Reajustar el tamaño del juego al cambiar el tamaño de la ventana
 window.addEventListener('resize', () => {
-    let newWidth = window.innerWidth;
-    let newHeight = window.innerHeight;
-    game.scale.resize(newWidth, newHeight);
-    game.scene.scenes.forEach(scene => {
-        if (scene.initGameObjects) {
-            scene.initGameObjects(scene.add.image(0, 0, 'background').setOrigin(0, 0));
-        }
-    });
+    if (window.innerWidth < 768) {
+        game.scale.resize(window.innerWidth, window.innerHeight * (window.innerWidth / 5760));
+    } else {
+        game.scale.resize(5760, window.innerHeight);
+    }
 });
